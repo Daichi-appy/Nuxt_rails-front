c7f5338322650a27935d1307db89723a5213197e
daichi.aoki <daichi.es47@gmail.com>
2021-12-08 13:26:54 +0900

    Create a component of delete-project as deleteModal.vue

--- pages/project/_id/dashboard.vue
+++ pages/project/_id/dashboard.vue
@@ -16,51 +16,18 @@
           <v-col
             align="center"
           >
-            <v-dialog v-model="dialog" max-width="600px">
-              <template v-slot:activator="{ on, attrs }">
-                <v-btn 
-                  outlined
-                  color="red"
-                  v-on="on"
-                  v-bind="attrs"
-                >
-                  削除する
-                </v-btn>
-              </template>
-
-              <v-card>
-                <v-container>
-
-                  <v-card-text>
-                    本当に削除しますか？
-                  </v-card-text>
-
-                  <v-card-actions>
-                    <v-btn
-                     color="blue" 
-                     @click="dialog = !dialog"
-                    >
-                    いいえ
-                    </v-btn>
-                    <v-btn 
-                      color="red" 
-                      @click="deleteProject"
-                      :loading="loading"
-                    >
-                      削除します
-                    </v-btn>
-                  </v-card-actions>
-
-                </v-container>
-              </v-card>
-
-            </v-dialog>
+            <delete-modal />
 
           </v-col>
         </v-row>
 
-
+        <!-- タスク一覧 -->
         <v-row class="mt-3">
+          <v-col
+            cols="3"
+          >
+            <modal />
+          </v-col>
           <v-col
             cols="3"
             v-for="(task, i) in recentTasks"
@@ -108,37 +75,7 @@ export default {
     }
   },
   methods: {
-    async deleteProject () {
-      // ローディング開始
-      this.loading = true
-      // 現在のプロジェクトののidを格納
-      const projectId = this.$store.state.current.project.id
-      // console.log(projectId)
-      const url = `/api/v1/projects/${projectId}`
 
-      // 削除リクエスト
-      await this.$axios
-          .delete(url)
-          .catch((err) => {
-            console.log(err)
-          })
-      // Apiからvuexにデータ更新
-      await this.replaceProject()
-      // 少し待つ
-      await this.resolveAfter(2)
-      // ローディング終了
-      this.loading = false
-      // ホーム画面に戻る
-      this.$router.push('/')
-    },
-    resolveAfter (sec) {
-      return new Promise(resolve => setTimeout(resolve, sec*1000))
-    },
-    replaceProject () {
-      this.$axios.$get('/api/v1/projects')
-        .then(response => this.$store.dispatch('getProjects', response))
-        .then(console.log("success replace"))
-    },
   }
 }
 </script>
